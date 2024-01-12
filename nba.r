2492fb16cebd1af64c3d6eab9107619ff9c1cb72
Richa Jain <jainricha127@gmail.com>
2024-01-11 20:38:19 -0500

    updates to r file

--- nba.r
+++ nba.r
@@ -1,1 +1,15 @@
-print("Hello World")
+library(tidyverse)
+library(gt)
+
+latest_raptor_player <- read.csv("datasets/latest_RAPTOR_by_player.csv")
+head(latest_raptor_player, 1)
+
+latest_raptor_team <- read.csv("datasets/latest_RAPTOR_by_team.csv")
+head(latest_raptor_team, 1)
+
+unique(latest_raptor_team$poss)
+latest_raptor_team[latest_raptor_team$poss == 431]
+
+#Note: We can't reproduce the table perfectly because the positions must have been manually added in 
+
+latest_raptor_player 
